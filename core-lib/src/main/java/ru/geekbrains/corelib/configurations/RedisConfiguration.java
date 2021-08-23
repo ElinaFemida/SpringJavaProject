6eb950b8e7d8a88eeb831258e9f7e36e446fb1e0
Elina <ya.ilina4@yandex.ru>
2021-08-23 18:22:54 +0300

    token checking added

---
+++ core-lib/src/main/java/ru/geekbrains/corelib/configurations/RedisConfiguration.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.corelib.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class RedisConfiguration {
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
